41704b3577d3e3556ab2b612c76dce3f6f9620cf
tcat <1232@qq.com>
2017-08-28 08:21:06 +0800

    哈

--- js/acg/loadacg.js
+++
@@ -1,8 +0,0 @@
-define([
-    'jquery',
-    'text!tpls/acgLoad.html'
-], function($, acgLoad) {
-    return function(){
-        $(".show").html(acgLoad);
-    }
-});
--- js/lib/animate.js
+++ js/lib/animate.js
@@ -1,85 +1,33 @@
-/**
- * Created by Lenovo on 2017/6/14.
- */
-function $(id){
-    return document.getElementById(id);
-}
-
-//显示元素；
-function show(ele){
-    ele.style.display = "block";
-}
-//隐藏元素；
-function hide(ele){
-    ele.style.display = "none";
-}
-
-
-
-//页面被卷去的部分方法封装
-function scroll(){
-    return {
-        top: window.pageYOffset || document.documentElement.scrollTop,
-        left: window.pageXOffset || document.documentElement.scrollLeft
-    }
-}
-
-
 //缓动框架
 function animate(ele,json,fn){
-    //清除定时器
+    //清楚定时器
     clearInterval(ele.timer);
     ele.timer = setInterval(function () {
-        //开闭原则：
         var bool = true;
         for(var k in json){
-            //操作属性在for循环中，特殊属性特殊处理；
-            //进行if判断；判断k的值；
-
-            if(k === "z-index"){//层级的：直接一步到位设置为目标值，不干涉到清除定时器；
-                ele.style.zIndex = json[k];
-
-            }else if(k === "opacity"){//透明度：1.小数(放大在缩小)；    2.兼容
-                //获取步长
-                //获取值的时候放大10/100方便运算，赋值的时候在缩小10/100;
-
-                //如果取值为0，按0算，如果取值为空字符串，按1算；
-                if(getStyle(ele,k) === "0"){
-                    var leader = 0;
-                }else{
-                    var leader = parseInt(getStyle(ele,k)*10) || 10;
-                }
-                var step = (parseInt(json[k]*10) - leader)/10;
-                //二次处理
+            //特殊属性特殊处理
+            if(k==="opacity"){
+                //小数计算*100后可以避免后面的向上取整向下取整。产的误差。
+                var leader = getStyle(ele,k)*100;//获取的是字符串
+                var step = (json[k]*100-leader)/10;
                 step = step>0?Math.ceil(step):Math.floor(step);
-                //赋值
-                leader = leader + step;
-                //透明度赋值：1.缩小10/100;   2.不带单位；   3.兼容ie678;
-                ele.style.opacity = leader/10;
-                //兼容ie678;
-                ele.style.filter = "alpha(opacity="+leader*10+")";
-
-                //没有到达指定位置，不允许清除定时器
-                if(json[k] !== leader/10){
+                ele.style.opacity = (leader + step)/100;
+                if(json[k] != getStyle(ele,k)){
                     bool = false;
                 }
-            }else{//正常属性；
-
-                //获取步长
-                var leader = parseInt(getStyle(ele,k)) || 0;
-                var step = (json[k] - leader)/10;
-                //二次处理
+            }else if(k==="z-index"){
+                ele.style.zIndex = json[k];
+            }else{
+                var leader = parseInt(getStyle(ele,k)) || 0;//获取的是字符串
+                var step = (json[k]-leader)/10;
                 step = step>0?Math.ceil(step):Math.floor(step);
-                //赋值
-                leader = leader + step;
-                ele.style[k] = leader + "px";
-                //没有到达指定位置，不允许清除定时器
-                if(json[k] !== leader){
+                ele.style[k] = leader + step + "px";
+                if(json[k] != parseInt(getStyle(ele,k))){
                     bool = false;
                 }
             }
+
         }
-        //清除定时器
         console.log(1);
         if(bool){
             clearInterval(ele.timer);
@@ -90,12 +38,19 @@ function animate(ele,json,fn){
     },30);
 }
 
-//获取的属性值为字符串
+//获取行内内嵌外链属性值（有单位）
 function getStyle(ele,attr){
-    //第一种
-    if(window.getComputedStyle){
-        return window.getComputedStyle(ele,null)[attr];
+    if(ele.currentStyle != undefined){
+        return ele.currentStyle[attr]
     }else{
-        return ele.currentStyle[attr];
+        return window.getComputedStyle(ele,null)[attr];
     }
+}
+
+
+function scroll(){
+    return {
+        "top": window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
+        "left": window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
+    };
 }
